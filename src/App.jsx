87ce0c476ab6b5e3944bb78edf66a30533662dbd
Sthefania <sthefanietorres827@gmail.com>
2025-06-20 16:26:24 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Footer from './components/footer.';
+import footer from "./components/Footer"
 export default function App() {
   return (
     <>
       <div>
         <h1>Hola mundo cruel</h1>
         <p>Vive la vidaaa y no dejes que la vida te viva</p>
+
         <footer/>
       </div>
     </>
