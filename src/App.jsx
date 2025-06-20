245593704222f29678ff8135c5079576e766f892
Sthefania <sthefanietorres827@gmail.com>
2025-06-20 14:59:21 -0500

    mi primer comit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1>Hola mundo cruel</h1>
+        <p>Vive la vidaaa y no dejes que la vida te viva</p>
+      </div>
+    </>
+  );
+}
+
+
