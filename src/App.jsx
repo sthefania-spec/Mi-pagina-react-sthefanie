12a834dca0d76f2cb12afa5e8fe5e612a5086cb8
Sthefania <sthefanietorres827@gmail.com>
2025-06-20 16:29:29 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
         <h1>Hola mundo cruel</h1>
         <p>Vive la vidaaa y no dejes que la vida te viva</p>
 
-        <footer/>
+        <Footer/>
       </div>
     </>
   );
